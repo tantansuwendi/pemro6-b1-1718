319000afa8ec032379ee95ff16a04a3560a4f66b
indramustholih <indramustholih@gmail.com>
2018-02-22 21:55:47 +0700

    tugas 1

---
+++ IndraM/DataTypeExample.java
@@ -0,0 +1,13 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+		System.out.println("Bilangan: " + bilangan);
+        System.out.println("Bilangan Pecahan: " + bilanganPecahan);
+        
+		// Tugas tamplkan bilangan dan bilangan pecahan
+  }
+}
