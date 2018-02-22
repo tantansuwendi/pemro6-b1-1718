85a0a9d4e093a217b39acb954f56c038636d3c68
wahyuwehaye <wehaye94@gmail.com>
2018-02-23 00:17:20 +0700

    Tugas 1

---
+++ wahyu/Tugas1_0617123020/src/tugas1_0617123020/Tugas1_0617123020.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1_0617123020;
+
+/**
+ *
+ * @author wehaye
+ */
+public class Tugas1_0617123020 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+        
+        System.out.println("Text : "+text);
+        System.out.println("Bilangan : "+bilangan);
+        System.out.println("Bilangan Pecahan : "+bilanganPecahan);
+    }
+    
+}
