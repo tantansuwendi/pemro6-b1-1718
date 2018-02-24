aa7a2c0c903e21554bd54a66c17617802c39cf9a
DitiaFajrin <ditia.fajrin@gmail.com>
2018-02-24 10:08:53 +0700

    Tambah file Tugas1

---
+++ ditia/DataTypeExample.java
@@ -0,0 +1,11 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example ditia";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+	System.out.println("Bilangan: " + bilangan);
+	System.out.println("Bilangan Pecahan: " + bilanganPecahan);	
+  }
+}
