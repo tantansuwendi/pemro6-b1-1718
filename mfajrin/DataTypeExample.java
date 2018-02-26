80d4f5a244d8b5091d40f342baa3b58af69a5e94
Mohammad Fajrin <mohammad.fajrin@hotmail.com>
2018-02-26 13:28:03 +0700

    tugas 1

---
+++ mfajrin/DataTypeExample.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypeexample;
+
+/**
+ *
+ * @author Diskominfo
+ */
+public class DataTypeExample {
+    public static void main(String[] args) {
+         String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+        System.out.println("Bilangan: " + bilangan);
+        System.out.println("Pecahan: " + bilanganPecahan);
+    }
+    
+}
