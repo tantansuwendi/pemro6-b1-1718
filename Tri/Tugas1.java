b55255aa491524b81915b0be3f9232603c55a1b1
Wicaksono <tri.wicaksono69@gmail.com>
2018-02-27 01:41:09 +0700

    tugas 1

---
+++ Tri/Tugas1.java
@@ -0,0 +1,16 @@
+public class DataTypeExample {
+/**
+*
+* @author Tri Wicaksono
+*/
+  public static void main(String[] args) {
+        String text = "Tri Tampan";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+        // Jawaban Tugas
+		System.out.println("Bilangan: " + bilangan);
+		System.out.println("Bilangan Pecahan: " + bilanganPecahan);
+  }
+}
