a05c1bc0315e4b44e7d7d6247eac962212e87be3
bechgun <gungun.iskandar@gmail.com>
2018-02-28 01:25:11 +0700

    Tambah tugas 1 java

---
+++ gungun/Tugas1.java
@@ -0,0 +1,14 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+         // Prints "text example"
+        System.out.println("Text: " + text);
+          // Prints bilangan
+        System.out.println("Bilangan: " + bilangan);
+         // Prints bilangan pecahan
+        System.out.println("Bilangan Pecahan: " + bilanganPecahan);	
+  }
+}
