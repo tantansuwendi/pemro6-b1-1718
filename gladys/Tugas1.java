226876ed45e814a6702f219bb0fb4b759e1f521b
gladysdara04 <gladysdara04@gmail.com>
2018-02-27 20:25:01 +0700

    Mengerjakan Tugas1

---
+++ gladys/Tugas1.java
@@ -0,0 +1,14 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+	//Nama	: Gladys Dara Nazariah
+	//NPM	: 0617123005
+
+	//Menampilkan bilangan dan bilangan pecahan
+        System.out.println("Bilangan: " + bilangan);
+	System.out.println("Bilangan Pecahan: " + bilanganPecahan);
+  }
+}
