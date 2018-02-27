840e0b2218667bd59cc4dd64eb886bb13b47c7a7
ranirahaa <ranirahay94@gmail.com>
2018-02-27 20:30:42 +0700

    Upload Tugas 1

---
+++ ranirahaa/DataTypeExample.java
@@ -0,0 +1,12 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+	System.out.println("bilangan: " + bilangan);
+	System.out.println("pecahan: " + pecahan);
+        // Tugas tamplkan bilangan dan bilangan pecahan
+  }
+}
