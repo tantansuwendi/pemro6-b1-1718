1e9de5252ca3d16ebe8f48a9cbf2ad5e6bda51c1
Desykartika21 <desykartika2104@gmail.com>
2018-02-27 20:53:53 +0700

    tugas pemro 6

---
+++ desykartika/tugas.java
@@ -0,0 +1,10 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+        // Tugas tamplkan bilangan dan bilangan pecahan
+  }
+}
