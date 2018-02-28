6d0d9b5daab183452e9797aee130fe77adb162af
Gun Gun Iskandar <gungun.iskandar@gmail.com>
2018-02-28 17:11:59 +0700

    Tugas1 Trisna Guys

---
+++ trisna/DataTypeExample.java
@@ -0,0 +1,16 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+        // Tugas tamplkan bilangan dan bilangan pecahan
+	System.out.println("bilangannya adalah guys" + bilangan);
+	System.out.println("bilangan pecahannya adalah guys" + bilanganPecahan);
+		
+  }
+}
+
+//Trisna Djuwanda Muharam
+//0617123011
