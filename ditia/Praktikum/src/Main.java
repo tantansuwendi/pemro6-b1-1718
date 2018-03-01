17abd13203dd5aeb3d80a74a1781cb3c8ae51009
DitiaFajrin <ditia.fajrin@gmail.com>
2018-03-01 20:27:44 +0700

    tambah file praktikum

---
+++ ditia/Praktikum/src/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DITIA
+ */
+public class Main {
+    public static final String KEY_ACCESS = "KEY_ACCESS"; //Pembuatan konstanta
+    public static final int NUM_DAYS_PER_YEARS = 365; //Pembuatan konstanta
+    public static void main(String[] args) {
+        System.out.println("Main Class");
+        
+        // Tipe data primitive 
+        // Bilangan
+        int bilanganBulat = 0;
+        double bilanganPecahan = 5.5;
+        int durasi = 2 * NUM_DAYS_PER_YEARS;
+        
+        //char
+        char karakter = 'A';
+        
+        //logika
+        boolean isEnabled = true;
+        
+        // Tipe data reference
+        Mahasiswa mhs = new Mahasiswa();
+        
+    }
+}
