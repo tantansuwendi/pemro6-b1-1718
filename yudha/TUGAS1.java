701b425816f3687009da85bf1720451db0fc882f
Unknown <yudhatambunan04@gmail.com>
2018-03-01 19:51:50 +0700

    TUGAS1

---
+++ yudha/TUGAS1.java
@@ -0,0 +1,12 @@
+public class TUGAS1 { 
+  public static void main(String[] args) { 
+        String text = "text example"; 
+        int bilangan = 8; 
+        double bilanganPecahan = 2.5; 
+ 
+        System.out.println("Text: " + text); 
+        // Tugas tamplkan bilangan dan bilangan pecahan 
+    System.out.println("Ini  bentuk bilangan : " + bilangan); 
+        System.out.println("Ini  bentuk bilangan pecahan : " + bilanganPecahan); 
+  } 
+}
