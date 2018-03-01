2b670e01e10ee80b6d764b38577da0c0e0b76468
Zaenal Muttaqin Abdussomad <zaenalmas93@gmail.com>
2018-03-01 20:22:34 +0700

    update

---
+++ zaenalmuttaqin/Praktikum/src/Mahasiswa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A43SD
+ */
+public class Mahasiswa {
+    //tipe data reference
+    String npm;
+    String nama ;
+    int usia;
+    
+}
