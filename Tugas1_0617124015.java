8e821bfc3037e673c9c8242b62d36da94289be6d
Aryadi Saputra <36506439+aryadisaputra@users.noreply.github.com>
2018-03-01 14:32:20 +0700

    Tugas 1

---
+++ Tugas1_0617124015.java
@@ -0,0 +1,12 @@
+public class DataTypeExample {
+  public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+        // Tugas tamplkan bilangan dan bilangan pecahan
+		System.out.println("Ini adalah bentuk bilangan : " + bilangan);
+        System.out.println("Ini adalah bentuk bilangan pecahan : " + bilanganPecahan);
+  }
+}
