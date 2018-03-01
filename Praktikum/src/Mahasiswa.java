1ed80594e81d0ee746688d2a0c9088a4347a24cd
kurnia <kurniaeliazar@gmail.com>
2018-03-01 20:22:32 +0700

    day 3 data type and primitive

---
+++ Praktikum/src/Mahasiswa.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ahmad Muhsin
+ */
+public class Mahasiswa {
+    String npm;
+    String nama;
+    int usia;
+}
