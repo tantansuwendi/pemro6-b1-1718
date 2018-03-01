683a7057accbf826f2e4365358e32afbcfe92b69
Zaenal Muttaqin Abdussomad <zaenalmas93@gmail.com>
2018-03-01 19:15:44 +0700

    masuk

---
+++ zaenalmuttaqin/Tugas1_0617123008 - Copy.java
@@ -0,0 +1,13 @@
+public class Tugas_1 {
+	public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+        System.out.println("bilangan: " + bilangan);
+        System.out.println("bilanganPecahan: " + bilanganPecahan);
+        System.out.print("bilangan dan bilanganPecahan : " + bilangan); System.out.println(" dan " + bilanganPecahan);
+        // Tugas tampilkan bilangan dan bilangan pecahan
+  }
+}
