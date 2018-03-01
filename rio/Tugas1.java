c3cf6b9096fb682ed75a1d99be597591aa81bb36
Rio Aidil <rioaidil@gmail.com>
2018-03-01 09:17:14 +0700

    Tugas 1 Rio

---
+++ rio/Tugas1.java
@@ -0,0 +1,14 @@
+public class Tugas1 {
+	public static void main(String[] args) {
+        String text = "text example";
+        int bilangan = 8;
+        double bilanganPecahan = 2.5;
+
+        System.out.println("Text: " + text);
+ 	// Tugas tampilkan bilangan dan bilangan pecahan
+        System.out.println("bilangan: " + bilangan);
+        System.out.println("bilanganPecahan: " + bilanganPecahan);
+        System.out.print("bilangan dan bilanganPecahan : " + bilangan); System.out.println(" dan " + bilanganPecahan);
+       
+  }
+}
