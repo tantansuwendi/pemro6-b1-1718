4ddf50186dd97f63bdc0ba6c6144a2c4c2408cd1
M Rizki <36356977+MRizki77@users.noreply.github.com>
2018-03-02 20:20:44 +0700

    Add files via upload

---
+++ Rizki/praktikum/src/Student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Student {
+    String NIK;
+    String Nama;
+    int Tgl_Lhr;
+    String Tmpt_Lhr;
+}
