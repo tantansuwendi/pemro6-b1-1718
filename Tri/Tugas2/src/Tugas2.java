63aaad717b15eba02408818b86f6fff7722d70bc
Kintamani <tri.wicaksono69@gmail.com>
2018-03-02 14:53:25 +0700

    Tugas2

---
+++ Tri/Tugas2/src/Tugas2.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author HP-GSeries Tri_Setyowicaksono
+ */
+public class Tugas2 { 
+    public static void main(String[] args) {
+        referensi atlit = new referensi();
+        
+        atlit.nama = "Bambang Tri Atmojo";
+        atlit.umur = 56;
+        atlit.tinggi=178.9;
+        
+        System.out.println(atlit.nama);
+        System.out.println(atlit.umur + " tahun");
+        System.out.println(atlit.tinggi + " cm");
+    }
+    
+}
---
+++ Tri/Tugas2/src/referensi.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ @author HP-GSeries Tri_Setyowicaksono
+ */
+public class referensi {
+    public int umur;
+    public String nama;
+    public double tinggi;
+}
