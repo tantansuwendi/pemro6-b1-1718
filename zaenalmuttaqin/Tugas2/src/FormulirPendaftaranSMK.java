4ead547cb6a647bd5eba6c0f3a9f6d9ee5e14a10
Zaenal Muttaqin Abdussomad <zaenalmas93@gmail.com>
2018-03-02 19:40:50 +0700

    update

---
+++ zaenalmuttaqin/Tugas2/src/FormulirPendaftaranSMK.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A43SD
+ */
+public class FormulirPendaftaranSMK {
+    String nama;
+    String ttgl;
+    String AsalSmp;
+    String NamaOrtu;
+    String Alamat;
+    String telp;
+    
+}
---
+++ zaenalmuttaqin/Tugas2/src/Tugas2_0617123008.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A43SD
+ */
+public class Tugas2_0617123008 {
+     public static void main(String[] args) {
+        System.out.println("Tugas 2 0617123008_Zaenal Muttaqin");
+                    
+        FormulirPendaftaranSMK formulir = new FormulirPendaftaranSMK();
+        formulir.nama = "Dilan";
+        formulir.ttgl = "Bandung, 25 Desember 1985";
+        formulir.AsalSmp ="SMP Karang Taruna";
+        formulir.Alamat="Jl. Ciawitali no 53 kp cisomplak";
+        formulir.NamaOrtu="Abidin";
+        formulir.telp = "0831324341";
+        
+
+      
+
+        System.out.println(" =====Formulir Pendaftaran====");
+        System.out.println("Nama                    : " + formulir.nama);
+        System.out.println("Tempat Tanggal Lahir    : " + formulir.ttgl);
+        System.out.println("Asal SMP                : " + formulir.AsalSmp);
+        System.out.println("Alamat                  : " + formulir.Alamat);
+        System.out.println("Nama Ortu               : " + formulir.NamaOrtu);
+        System.out.println("No Telpon               : " + formulir.telp);
+        
+        
+        
+    }
+    
+}
