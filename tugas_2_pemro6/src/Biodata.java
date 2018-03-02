0494f00958cdcce7ef0649c29d268af192641203
Taufik Rizal Pahlawi <36269151+taufikrizalp@users.noreply.github.com>
2018-03-02 19:50:44 +0700

    Tugas2_Pemro6
    Tugas 2 Pemrograman 6 (OOP)

---
+++ tugas_2_pemro6/src/Biodata.java
@@ -0,0 +1,26 @@
+
+public class Biodata {
+    public static void main(String[] args) {
+        type_data mhs = new type_data();
+        mhs.npm = 0617123006;
+        mhs.nama = "Taufik Rizal Pahlawi";
+        mhs.tempat_lahir = "Jakarta";
+        mhs.tanggal_lahir = "06 Februari 1995";
+        mhs.alamat = "Komplek Griya Cipatik Indah No 16";
+        mhs.agama = "Islam";
+        mhs.status = "Belum Menikah";
+        mhs.pekerjaan = "Mahasiswa";
+        System.out.println("Biodata");
+        System.out.println("NPM           :" + mhs.npm);
+        System.out.println("Nama Lengkap  :" + mhs.nama);
+        System.out.println("Tempat Lahir  :" + mhs.tempat_lahir);
+        System.out.println("Tanggal Lahir :" + mhs.tanggal_lahir);
+        System.out.println("Alamat        :" + mhs.alamat);
+        System.out.println("Agama         :" + mhs.agama);
+        System.out.println("Status        :" + mhs.status);
+        System.out.println("Pekerjaan     :" + mhs.pekerjaan);
+        
+        /*private static final String KEY_ACCESS = "KEY_ACCESS";*/
+        // type data yang terdiri dari gabungan type data primitive
+    }
+}
---
+++ tugas_2_pemro6/src/type_data.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class type_data {
+    int npm;
+    String nama, tempat_lahir, tanggal_lahir, alamat, agama, status, pekerjaan;
+}
