d0fd2eedb19cfafbe5790b7afcb6ec069c09ebb7
Trisnadjuwandamuharam <36268928+Trisnadjuwandamuharam@users.noreply.github.com>
2018-03-06 19:53:11 +0700

    Add files via upload
    Tugas2

---
+++ trisna/Tugas_2/src/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+
+/**
+ *
+ * @author Trisna
+ */
+public class Main {
+ 
+    
+    public static void main(String[] args) {
+        
+
+Warga wg = new Warga();
+wg.NIK = "0617123011";
+wg.Nama = "Trisna";
+wg.Tgl_Lhr = 7;
+wg.Tmpt_Lhr = "garut";
+
+
+
+System.out.println("Ini Data Warga");
+System.out.println("Nama : " + wg.NIK);
+System.out.println("NPM  : " + wg.Nama);
+System.out.println("Usia : " + wg.Tgl_Lhr);
+System.out.println("Usia : " + wg.Tmpt_Lhr);
+
+    }
+}
