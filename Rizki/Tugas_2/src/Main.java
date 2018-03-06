5c6ef4891f7faf21f6a79e986f9a5c70aaa79bd3
M Rizki <36356977+MRizki77@users.noreply.github.com>
2018-03-06 19:11:26 +0700

    Add files via upload
    Tugas 2

---
+++ Rizki/Tugas_2/src/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+
+/**
+ *
+ * @author Rizki
+ */
+public class Main {
+ 
+    
+    public static void main(String[] args) {
+        
+
+Student std = new Student();
+std.NIK = "060606";
+std.Nama = "0617123002";
+std.Tgl_Lhr = 25;
+std.Tmpt_Lhr = "singaparna";
+
+
+
+System.out.println("Ini Data Student");
+System.out.println("Nama : " + std.NIK);
+System.out.println("NPM  : " + std.Nama);
+System.out.println("Usia : " + std.Tgl_Lhr);
+System.out.println("Usia : " + std.Tmpt_Lhr);
+
+    }
+}
