de257f40fb9901c1b197828d86d933f82e4e1827
Nur Rohman Fajri <nurrohmanfajri90@gmail.com>
2018-03-08 16:34:42 +0700

    Tugas 1

---
+++ nurfajri/DataTypeExample.java
@@ -0,0 +1,16 @@
+/**
+ * Nama		: Nur Rohman Fajri
+ * NPM 		: 0617123007
+ *
+ * Tugas tamplkan bilangan dan bilangan pecahan
+ */
+ 
+public class DataTypeExample {
+	public static void main(String[] args) {
+		int bilangan = 8;
+		double bilanganPecahan = 2.5;
+
+		System.out.println("Bilangan: " + bilangan);
+		System.out.println("Bilangan Pecahan: " + bilanganPecahan);
+	}
+}
