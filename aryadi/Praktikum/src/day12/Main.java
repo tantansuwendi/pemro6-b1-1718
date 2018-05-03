ece27a1bf77f3739a49cce40f64830549b6a22e5
Aryadi Saputra <36506439+aryadisaputra@users.noreply.github.com>
2018-05-03 20:36:22 +0700

    Day 12 - Canvas
    Day 12

---
+++ aryadi/Praktikum/src/day12/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day12;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyCanvas cvs = new MyCanvas();
+        Frame f = new Frame ("My Canvas");
+        f.add(cvs);
+        f.setSize(500, 500);
+        f.setVisible(true);
+    }
+}
---
+++ aryadi/Praktikum/src/day12/MyCanvas.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day12;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author STUDENT
+ */
+public class MyCanvas extends Canvas {
+    
+    public MyCanvas() {
+        setSize(500, 500);
+    }
+    
+    @Override
+    public void paint (Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(250, 250, 100, 100);
+        
+        g.setColor(Color.RED);
+        g.drawRect(125, 250, 50, 50);
+        
+        g.setColor(new Color(0,0,255));
+        g.fillOval(375, 125, 50, 50);
+        
+        int x[] = {20, 40, 60, 80, 100, 120, 140, 140, 20};
+        int y[] = {250, 125, 250, 125, 250, 125, 250, 375, 375};
+        
+        g.fillPolygon(x, y, 9);
+    }
+}
